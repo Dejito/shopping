fad5db494aaea5309af49727e4f7f7d69ba2da16
dejito <Deerealboy@gmail.com>
2026-01-20 17:20:10 +0100

    add products image assets

---
+++ lib/data/app_asets.dart
@@ -0,0 +1,12 @@
+
+class AppImages {
+  static const forty = 'assets/images/forty.webp';
+  static const iMate = 'assets/images/imate_stores.webp';
+  static const iPhone12 = 'assets/images/iPhone12Pro.webp';
+  static const iPhoneXs = 'assets/images/iPhone_Xs.webp';
+  static const nokiaG20 = 'assets/images/nokia-g20.webp';
+  static const ogaBassey = 'assets/images/ogabassey.webp';
+  static const okayPhone = 'assets/images/okay_fones.webp';
+  static const soundCore = 'assets/images/sound_core.webp';
+
+}
