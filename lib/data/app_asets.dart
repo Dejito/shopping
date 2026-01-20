c15c0c69cbff6ad809c25f8d604be3c195c6af6b
dejito <Deerealboy@gmail.com>
2026-01-20 21:56:39 +0100

    populating merchants list in progress

--- lib/data/app_asets.dart
+++ lib/data/app_asets.dart
@@ -8,5 +8,16 @@ class AppImages {
   static const ogaBassey = 'assets/images/ogabassey.webp';
   static const okayPhone = 'assets/images/okay_fones.webp';
   static const soundCore = 'assets/images/sound_core.webp';
+  static const casper = 'assets/images/casper.webp';
+  static const dw = 'assets/images/dw.webp';
+  static const hubMart = 'assets/images/hubmart.webp';
+  static const justPhones = 'assets/images/just_fones.webp';
+  static const justUsed = 'assets/images/just_used.webp';
+  static const justrite = 'assets/images/justrite.webp';
+  static const orile = 'assets/images/orile.webp';
+  static const pointek = 'assets/images/pointek.webp';
+  static const slot = 'assets/images/slot.webp';
+  // static const soundCore = 'assets/images/sound_core.webp';
+  // static const soundCore = 'assets/images/sound_core.webp';
 
 }
