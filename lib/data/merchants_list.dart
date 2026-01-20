f812340f1eef79c3e7c323fc3e75ecb6d4d3e19c
dejito <Deerealboy@gmail.com>
2026-01-20 20:28:50 +0100

    populating merchants list

---
+++ lib/data/merchants_list.dart
@@ -0,0 +1,91 @@
+
+import 'dart:ui';
+
+import 'package:credpal_shopping/data/models/merchant_model.dart';
+
+final merchants = [
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Justrite',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Orile Restaurant',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Slot',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Pointek',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'ogabassey',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Casper Stores',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Dreamworks ',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Justrite',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Justrite',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Hubmart',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Just Used',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+  MerchantModel(
+    isOnline: true,
+    brandColor: const Color(0xFF000065),
+    brandName: 'Just fones',
+    brandLogo: 'assets/images/oga_bassey.png',
+  ),
+
+
+];
