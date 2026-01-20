4ee72058a55cee226f9ef38f2988950d6b265335
dejito <Deerealboy@gmail.com>
2026-01-20 17:20:34 +0100

    adding products listing

---
+++ lib/data/products.dart
@@ -0,0 +1,56 @@
+import 'package:credpal_shopping/data/app_asets.dart';
+import 'package:credpal_shopping/data/models/product_model.dart';
+
+final products = [
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+  ProductModel(
+      merchantLogo: AppImages.forty,
+      productImage: AppImages.nokiaG20,
+      productName: 'Nokia G20',
+      productPrice: '39,780',
+      productSlashedPrice: '295,999'
+  ),
+
+
+];
