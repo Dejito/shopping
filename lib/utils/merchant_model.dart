add92d5cb2b564bfeb991f3bb4057483fc6ccd11
dejito <Deerealboy@gmail.com>
2026-01-20 16:53:44 +0100

    add merchant model

---
+++ lib/utils/merchant_model.dart
@@ -0,0 +1,17 @@
+
+import 'dart:ui';
+
+class MerchantModel {
+
+  final bool isOnline;
+  final Color brandColor;
+  final String brandName;
+  final String brandLogo;
+
+  MerchantModel({
+    required this.isOnline,
+    required this.brandColor,
+    required this.brandName,
+    required this.brandLogo});
+
+}
